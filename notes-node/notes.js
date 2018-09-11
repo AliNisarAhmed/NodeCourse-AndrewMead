b33f36b118d27d18f02d2f3216d2873b3bb4b2af
AliNisarAhmed <ali.nisar87@gmail.com>
2018-09-11 20:13:32 +0500

    adding notes to local json file - except duplicates

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,27 @@
 console.log('starting notes.js');
 
+const fs = require('fs');
+
 const addNote = (title, body) => {
-  console.log('adding note ', title, body);
+  let notes = [];
+  const note = {
+    title,
+    body
+  };
+
+  try {
+    const notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (error) {
+    
+  }
+
+  const duplicateNotes = notes.filter(note => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 const getAll = () => {
