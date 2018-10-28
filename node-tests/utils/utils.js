4d9253f352eaa29e75dbc3497a128a85cb6145b6
AliNisarAhmed <ali.nisar87@gmail.com>
2018-10-28 11:49:01 +0500

    tests using jest

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,1 +1,22 @@
 module.exports.add = (a, b) => a + b;
+
+module.exports.square = (x) => x * x;
+
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b);
+  }, 2000)
+}
+
+module.exports.asyncSquare = (a, callback) => {
+  setTimeout(() => {
+    callback(a * a);
+  }, 1000);
+}
+
+module.exports.obj = {
+  name: 'ali',
+  surname: 'ahmed',
+  age: 31,
+  hobbies: 'learning JS'
+};
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,7 +2,29 @@ const utils = require('./utils');
 
 it('adds two values', function() {
   var res = utils.add(11, 33);
-  if (res !== 44) {
-    throw new Error('Expected 44, but got something else');
-  }
+  expect(res).toBe(44);
 });
+
+it('squares a number', () => {
+  var res = utils.square(4);
+  expect(res).toBe(16);
+});
+
+test('checks the object for specific properties', () => {
+  let res = utils.obj;
+  expect(res).toHaveProperty('name', 'ali');
+});
+
+test('should async add two numbers', (done) => {
+  utils.asyncAdd(3, 4, (sum) => {
+    expect(sum).toBe(7);
+    done();
+  })
+})
+
+test('should async square', (done) => {
+  utils.asyncSquare(4, (square) => {
+    expect(square).toBe(16);
+    done();
+  })
+})
