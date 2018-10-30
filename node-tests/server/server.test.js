bf89a83ea7a6aa21fdd7b2e903d71839afeea466
AliNisarAhmed <ali.nisar87@gmail.com>
2018-10-30 23:30:47 +0500

    using describe

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -1,26 +1,28 @@
 const request = require('supertest');
 const app = require('./server').app;
 
-test('should return the correct response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect(res => {
-      expect(res.body).toMatchObject({
-        error: 'Page not found.'
+describe('Testing async functions', () => {
+  test('should return the correct response', (done) => {
+    request(app)
+      .get('/')
+      .expect(404)
+      .expect(res => {
+        expect(res.body).toMatchObject({
+          error: 'Page not found.'
+        })
       })
-    })
-    .end(done);
-});
-
-test('a specific user exists', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect(res => {
-      expect(res.body).toEqual(expect.arrayContaining([
-        {name: 'ali', age: 31}
-      ]));
-    })
-    .end(done)
-});
+      .end(done);
+  });
+  
+  test('a specific user exists', (done) => {
+    request(app)
+      .get('/users')
+      .expect(200)
+      .expect(res => {
+        expect(res.body).toEqual(expect.arrayContaining([
+          {name: 'ali', age: 31}
+        ]));
+      })
+      .end(done)
+  });
+})
