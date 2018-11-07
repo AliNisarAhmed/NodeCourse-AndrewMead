73a3e485e7c31dcbb97e8f7e9530f8f2ba49fca7
AliNisarAhmed <ali.nisar87@gmail.com>
2018-11-07 18:22:34 +0500

    using mongodb native library for querying and fetching

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+
+const { MongoClient, ObjectID } = require('mongodb')
+
+const obj = new ObjectID();
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err) { return console.log('unable to connect to MongoDB Server'); }
@@ -15,14 +19,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // })
 
-  db.collection("Users").insertOne({
-    name: 'Ali',
-    age: 31,
-    location: 'Karachi'
-  }, (err, result) => {
-    if (err) { return console.log(err) }
-    console.log(JSON.stringify(result.ops, undefined, 2));
+  // db.collection("Todos").find({ 
+  //   _id: new ObjectID("5be2e39b521b9b253d2825f7")  
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, null, 2));
+  // }, (err) => {
+  //   console.log('unable to fetch todos', err)
+  // })
+
+  db.collection("Users").find({name: "Kyle Simpson"}).toArray().then((docs) => {
+    console.log('Documents');
+    console.log(JSON.stringify(docs, null, 2));
+  }, (err) => {
+    console.log('unable to fetch todos', err)
   })
 
-  client.close();
 });
